9a9474ce23b7f4725785d29e1732f355f0f5936a
Francisco Sosa <fransosa.dev@gmail.com>
2022-10-14 20:35:30 -0300

    1st commit

---
+++ Assets/Scripts/Fichaje.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fichaje : MonoBehaviour {
+
+    public InputField codigoIF;
+    public Text nombreProductoTxt;
+    public Text precioUnitarioTxt;
+    public InputField cantidadIF;
+    public Text subtotalTxt;
+    public Button ficharBtn;
+
+    private void Start() {
+        
+
+    }
+
+}
