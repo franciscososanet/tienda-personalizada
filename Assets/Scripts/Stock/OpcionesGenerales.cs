212d152012faea80a03f83d3c940bc0c5b989672
Francisco Sosa <fransosa.dev@gmail.com>
2023-01-05 15:28:35 -0300

    Agregar nueva mercaderia (sin validaciones)

--- Assets/Scripts/Stock/OpcionesGenerales.cs
+++ Assets/Scripts/Stock/OpcionesGenerales.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class OpcionesGenerales : MonoBehaviour {
-    
+
+    public ConexionDB DBScript;    
     public CambiarEscena CambiarEscenaScript;
 
     public GameObject panelOpcionesGenerales;
@@ -19,7 +21,7 @@ public class OpcionesGenerales : MonoBehaviour {
         botonesContainer.GetChild(1).GetComponent<Button>().onClick.RemoveAllListeners();
         botonesContainer.GetChild(2).GetComponent<Button>().onClick.RemoveAllListeners();
 
-        botonesContainer.GetChild(0).GetComponent<Button>().onClick.AddListener(CrearProducto);
+        botonesContainer.GetChild(0).GetComponent<Button>().onClick.AddListener(MostrarPanelCrearProducto);
         botonesContainer.GetChild(1).GetComponent<Button>().onClick.AddListener(VerRegistros);
         botonesContainer.GetChild(2).GetComponent<Button>().onClick.AddListener(VolverAlInicio);
 
@@ -29,9 +31,40 @@ public class OpcionesGenerales : MonoBehaviour {
         panelOpcionesGenerales.SetActive(false);
     }
 
-    private void CrearProducto(){
-        Debug.Log("Hola, aca crearia un producto nuevo...");
+    #region Opción: Crear Producto
+
+    public GameObject panelCrearProducto;
+    public InputField codigoIF;
+    public InputField nombreProductoIF;
+    public InputField precioUnitarioIF;
+    public InputField promocionIF;
+    public InputField stockIF;
+    public InputField unidadesVendidasIF;
+    public InputField fechaCreacionIF;
+    public InputField proveedorIF;
+    public Button crearProductoBtn;
+
+    private void MostrarPanelCrearProducto(){
+
+        GameObject ifsContainer = codigoIF.transform.parent.parent.gameObject;
+        
+        panelCrearProducto.SetActive(true);
+
+        crearProductoBtn.onClick.RemoveAllListeners();
+        crearProductoBtn.onClick.AddListener(CrearProducto);
+
     }
+
+    private void CrearProducto(){
+
+        DBScript.AbrirDB();
+        DBScript.dbCommand = DBScript.dbConnection.CreateCommand();
+        string sqlQuery = String.Format("INSERT INTO Mercaderia (codigo, producto, preciounitario, promocion, stock, 'unidades vendidas', 'fecha de creacion', proveedor) VALUES (\"{0}\", \"{1}\", \"{2}\", \"{3}\", \"{4}\", \"{5}\", \"{6}\", \"{7}\")", codigoIF.text, nombreProductoIF.text, precioUnitarioIF.text, promocionIF.text, stockIF.text, unidadesVendidasIF.text, fechaCreacionIF.text, proveedorIF.text);  
+        DBScript.dbCommand.CommandText = sqlQuery;
+        DBScript.dataReader = DBScript.dbCommand.ExecuteReader();
+    }
+
+    #endregion
 
     private void VerRegistros(){
         Debug.Log("Aca veria los registros...");
